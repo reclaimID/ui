a69359805e39b56b437f4cd285ddbc377a6e8396
Schanzenbach, Martin <martin.schanzenbach@aisec.fraunhofer.de>
2019-01-31 18:21:58 +0100

    update

--- src/app/gns.service.ts
+++ src/app/gns.service.ts
@@ -11,7 +11,7 @@ export class GnsService {
 
   getClientName(client_id) {
     return this.http.get<any[]>(this.config.get().apiUrl 
-      + '/gns/+.'+client_id+'?record_type=RECLAIM_OIDC_CLIENT');
+      + '/gns/@.'+client_id+'?record_type=RECLAIM_OIDC_CLIENT');
   }
 
 }
