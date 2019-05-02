009a21c944d7d80b98f07c4647f1a7ce2649e8d6
Schanzenbach, Martin <mschanzenbach@posteo.de>
2019-05-02 15:11:37 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { IdentityListComponent } from './identity-list/identity-list.component';
 
 const routes: Routes = [
-    { path: 'identities', component: IdentityListComponent },
-    { path: '', redirectTo: '/identities', pathMatch: 'full' },
+    { path: '', component: IdentityListComponent },
+    //{ path: 'identities', component: IdentityListComponent },
+    //{ path: '', redirectTo: '/identities', pathMatch: 'full' },
 ];
 
 @NgModule({
