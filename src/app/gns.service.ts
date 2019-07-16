0a920a5c49f73488eed9d384b631070060ec85de
Schanzenbach, Martin <mschanzenbach@posteo.de>
2019-07-16 17:47:44 +0200

    timeout

--- src/app/gns.service.ts
+++ src/app/gns.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { retry } from 'rxjs/operators';
+import { timeout } from 'rxjs/operators';
 
 
 import { ConfigService } from './config.service';
@@ -12,6 +12,6 @@ export class GnsService {
 
   getClientName(client_id) {
     return this.http.get<any>(this.config.get().apiUrl
-      + '/gns/' + client_id + '?record_type=RECLAIM_OIDC_CLIENT');
+      + '/gns/' + client_id + '?record_type=RECLAIM_OIDC_CLIENT').pipe(timeout(30000));
   }
 }
