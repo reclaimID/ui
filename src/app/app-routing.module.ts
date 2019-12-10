06a374032147df1112b221d70d2acde92e65ee11
Schanzenbach, Martin <mschanzenbach@posteo.de>
2019-12-10 20:08:57 +0100

    more refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { IdentityListComponent } from './identity-list/identity-list.component';
 import { NewIdentityComponent } from './new-identity/new-identity.component';
 import { EditIdentityComponent } from './edit-identity/edit-identity.component';
+import { AuthorizationRequestComponent } from './authorization-request/authorization-request.component';
 
 const routes: Routes = [
-    { path: 'index.html', component: IdentityListComponent },
+    { path: '', component: IdentityListComponent },
     // { path: 'identities', component: IdentityListComponent },
-    { path: '', redirectTo: '/index.html', pathMatch: 'full' },
+    //{ path: '', redirectTo: '/index.html', pathMatch: 'full' },
     { path: 'new-identity', component: NewIdentityComponent },
     { path: 'edit-identity/:id', component: EditIdentityComponent },
+    { path: 'authorization-request', component: AuthorizationRequestComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: false })],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
