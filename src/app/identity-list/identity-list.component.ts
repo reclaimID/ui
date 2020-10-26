af8a888a43da9f54ec388c97bbaa43d296130784
anna wimbauer <anna.wibauer@gmx.de>
2020-10-26 16:08:44 +0100

    clean up

--- src/app/identity-list/identity-list.component.ts
+++ src/app/identity-list/identity-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, UrlHandlingStrategy } from '@angular/router';
+import { ActivatedRoute, Router} from '@angular/router';
 
 import { Attribute } from '../attribute';
 import { Credential } from '../credential';
