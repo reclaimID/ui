b5930110b3014fcca80702f4204a490fc1c0803d
Martin Schanzenbach <schanzen@gnunet.org>
2020-12-27 23:39:51 +0900

    fix import for browser plugin, other import may be broken

--- src/app/identity-list/identity-list.component.ts
+++ src/app/identity-list/identity-list.component.ts
@@ -65,7 +65,7 @@ export class IdentityListComponent implements OnInit {
       localStorage.setItem('credentialState', this.route.snapshot.queryParams["state"]);
       localStorage.setItem('credentialSession_State', this.route.snapshot.queryParams["session_state"]);
       var user = localStorage.getItem('userForCredential');
-      this.router.navigate(['/import-attributes', user]);
+      this.router.navigate(['/edit-identity', user]);
     }
     if (!this.oidcService.inOpenIdFlow() && undefined == this.route.snapshot.queryParams["authz_request"]) {
       this.oidcService.parseRouteParams(this.route.snapshot.queryParams);
